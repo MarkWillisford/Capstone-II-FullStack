61eaa07f8b4b5b834e01e2ac88f9fce4846a874c
Mark Willisford <MarkWillisford@users.noreply.github.com>
2018-08-14 12:10:21 -0700

    adding routes

--- test/test-routes.js
+++ test/test-routes.js
@@ -99,3 +99,141 @@ describe('Signup Screen', function(){
 			});
 	});
 });
+
+describe('Shift Input Screen', function(){
+	before(function(){
+		return runServer();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	// Test Strategy:
+	// 1. make request to root
+	// 2. ensure return object is HTML with status code 200
+	it('should return HTML and status code 200', function(){
+		return chai
+			.request(app)
+			.get('/input_shift.html')
+			.then(function(res){
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+	});
+});
+
+describe('Paycheck Input Screen', function(){
+	before(function(){
+		return runServer();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	// Test Strategy:
+	// 1. make request to root
+	// 2. ensure return object is HTML with status code 200
+	it('should return HTML and status code 200', function(){
+		return chai
+			.request(app)
+			.get('/input_paycheck.html')
+			.then(function(res){
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+	});
+});
+
+describe('Paycheck Varification Screen', function(){
+	before(function(){
+		return runServer();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	// Test Strategy:
+	// 1. make request to root
+	// 2. ensure return object is HTML with status code 200
+	it('should return HTML and status code 200', function(){
+		return chai
+			.request(app)
+			.get('/paycheck_varification.html')
+			.then(function(res){
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+	});
+});
+
+describe('Settings Screen', function(){
+	before(function(){
+		return runServer();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	// Test Strategy:
+	// 1. make request to root
+	// 2. ensure return object is HTML with status code 200
+	it('should return HTML and status code 200', function(){
+		return chai
+			.request(app)
+			.get('/settings.html')
+			.then(function(res){
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+	});
+});
+
+describe('Graphs Screen', function(){
+	before(function(){
+		return runServer();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	// Test Strategy:
+	// 1. make request to root
+	// 2. ensure return object is HTML with status code 200
+	it('should return HTML and status code 200', function(){
+		return chai
+			.request(app)
+			.get('/graphs.html')
+			.then(function(res){
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+	});
+});
+
+describe('Tables Screen', function(){
+	before(function(){
+		return runServer();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	// Test Strategy:
+	// 1. make request to root
+	// 2. ensure return object is HTML with status code 200
+	it('should return HTML and status code 200', function(){
+		return chai
+			.request(app)
+			.get('/tables.html')
+			.then(function(res){
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			});
+	});
+});
