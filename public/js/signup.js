cf73fa34ec598a2f215ec97ad768d502e912273e
Mark Willisford <MarkWillisford@users.noreply.github.com>
2018-08-16 13:48:57 -0700

    combining

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,28 @@
 'use strict';
 
-$(function() {
-    $('.submitButton').click(() => {
+
+function myFunction() {
+    alert($('.signupEmailText').val());
+    $.ajax({
+        url: '/users/signup.html',
+        method: 'POST',
+        data: {
+            email: `${$('.signupEmailText').val()}`,
+            username: `${$('.signupUserNameText').val()}`,
+            password: `${$('.signupPasswordText').val()}`,
+        },
+        success: (response) => {
+            sessionStorage.setItem('token', response.token);
+            location.href = '/home.html';
+        }
+    })
+};
+
+
+/*
+$(function myfunction() {
+    //$('.submitButton').click(() => {
     	console.log($('.signupEmailText').text);
-    	/*
         $.ajax({
             url: '/users/signup.html',
             method: 'POST',
@@ -16,6 +35,5 @@ $(function() {
                 location.href = '/protected.html';
             }
         })
-        */
-    })
-})
+    //})
+})*/
